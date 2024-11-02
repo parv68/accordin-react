9c8f479f3ff192728d8a32437e1dcc0396d77637
Parv <parvruhil68@gmail.com>
2024-11-02 15:57:19 +0530

    data file

---
+++ src/data.jsx
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id : '1',
+        question : "What are accorion components?",
+        answer : "Accordions are a common UI component that displays a series of collapsible sections, with the ability to expand or collapse each section individually."
+    },
+    {
+        id : '2',
+        question : "What are they used for?",
+        answer : "They are commanly employed in varoius context, including FAQs, product description"
+    },
+    {
+        id : '3',
+        question : "How do they work?",
+        answer : "Accordions are built using HTML, CSS, and JavaScript. They are typically implemented with nested divs and CSS transitions."
+    },
+    {
+        id : '4',
+        question : "What are some common accessibility features?",
+        answer : "Accordions are accessible to users with visual impairments by default. They can be made keyboard accessible by adding appropriate ARIA roles and attributes."
+    }
+]
+export default data
